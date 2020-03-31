e73a94ee61fd8fccc0a2fbbc89e6263b96cd63ff
YangJ0605 <1442122744@qq.com>
2020-04-01 00:11:16 +0800

    npm publish

---
+++ index.js
@@ -0,0 +1,9 @@
+import Button from './src/components/button.vue'
+import ButtonGroup from './src/components/button-group.vue'
+import Icon from './src/components/icon.vue'
+
+export {
+  Button,
+  ButtonGroup,
+  Icon
+}
