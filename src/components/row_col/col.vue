5a4edf526ef3daf47f6e61af60dea63025b55585
YangJ0605 <1442122744@qq.com>
2020-04-02 23:47:35 +0800

    搭建基本结构

---
+++ src/components/row_col/col.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
+
+<style lang='scss'>
+.col {
+  height: 100px;
+  background-color: blue;
+  width: 50%;
+  border: 1px solid red;
+}
+</style>
---
+++ src/components/row_col/row.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
