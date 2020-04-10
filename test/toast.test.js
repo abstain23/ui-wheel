700aec5918efa4915a3bea812555572bf4464372
YangJ0605 <1442122744@qq.com>
2020-04-10 11:52:43 +0800

    完成toast测试用例

---
+++ test/toast.test.js
@@ -0,0 +1,76 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Toast from '../src/components/toast/toast.vue'
+
+
+describe('Toast', () => {
+  it('存在', () => {
+    expect(Toast).to.exist
+  })
+
+  describe('属性', () => {
+    // this.timeout(15000)
+    it('自动关闭 autoClose autoCloseDelay', (done) => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          autoClose: true,
+          autoCloseDelay: 1000
+        }
+      }).$mount(div)
+      vm.$on('beforeClose', () => {
+        // console.log('close')
+        expect(document.body.contains(vm.$el)).to.eq(false)
+        done()
+      })
+      // setTimeout(() => {
+      //   expect(document.body.contains(vm.$el)).to.eq(false)
+      //   done()
+      // }, 1500)
+    })
+
+    it('测试closeButton', () => {
+      const Constructor = Vue.extend(Toast)
+      const callback = sinon.fake() //测试事件
+      const vm = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '测试关闭',
+            callback
+          }
+        }
+      }).$mount()
+      // console.log(vm.$el.outerHTML)
+      const closeButton = vm.$el.querySelector('.close')
+      expect(closeButton.innerHTML).to.eq('测试关闭')
+      closeButton.click()
+      expect(callback).to.have.been.called
+    })
+
+
+    it('enableHtml', () => {
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          enableHtml: true
+        }
+      })
+      vm.$slots.default = '<h1 id="cc">hhhh</h1>'
+      vm.$mount()
+      const h1 = vm.$el.querySelector('#cc')
+      expect(h1).to.exist
+    })
+
+    it('position', () => {
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          position: 'top'
+        }
+      }).$mount()
+      expect(vm.$el.classList.contains('position-top')).to.eq(true)
+    })
+  })
+})
